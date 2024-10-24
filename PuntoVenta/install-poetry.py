7915aaa7a0d3e183eb0dcd3217659418533b6ffd
Kevin Cax <caxkevin86gmail.com>
2024-10-24 14:32:05 -0600

    Regresando a versión anterior

--- PuntoVenta/install-poetry.py
+++
@@ -1,7 +0,0 @@
-# Copia y pega el siguiente contenido en un archivo llamado install-poetry.py
-import sys
-import subprocess
-
-if __name__ == "__main__":
-    url = "https://install.python-poetry.org"
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "poetry", "--upgrade"])
